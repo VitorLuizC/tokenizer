12bb10d9da608fbc3f609442ea94e179d1f37713
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2024-12-19 00:33:49 -0300

    🐛 fix SymbolToken missing in the mod.ts

--- tokens.ts
+++ tokens.ts
@@ -1,3 +1,4 @@
 export { NumberToken } from './tokens/NumberToken.ts'
+export { SymbolToken } from './tokens/SymbolToken.ts'
 export { UnknownToken } from './tokens/UnknownToken.ts'
 export { WhiteSpaceToken } from './tokens/WhiteSpaceToken.ts'
